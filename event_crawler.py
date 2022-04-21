dbd5c9a36423bf75ef705207a26590b0e7254dd3
Seunghyun-Kang <kiseyno@gmail.com>
2022-04-21 15:31:31 +0900

    에어서울 이벤트 정보 크롤링 로직 추가

--- event_crawler.py
+++ event_crawler.py
@@ -51,7 +51,7 @@ class EventCrawler:
                 return
         elif "airbusan" in self.driver.current_url:
             print("This is AirBusan events :")
-            tag = "jejuair"
+            tag = "airbusan"
             event_list = self.driver.find_elements(by=By.XPATH, value="//ul[@id='nav_2']//li//a")
             print(f"Find {len(event_list)} lists of event")
 
@@ -97,9 +97,38 @@ class EventCrawler:
                     print(df)
                 else:
                     return
+        elif "airseoul" in self.driver.current_url:
+            print("This is Airseoul events :")
+            tag = "airseoul"
+            time.sleep(3)
+            page_buttons = self.driver.find_elements(by=By.XPATH, value="//ul[@class='page_navi']//li")
+            page_num = len(page_buttons)
+            print(f"Total page in Airseoul event site : {page_num}")
+            
+            df = pd.DataFrame(columns=['airline', 'url', 'date',  'header','content']) 
+            for page in range(page_num):
+                self.driver.get(url = f"https://flyairseoul.com/CW/ko/ingEvent.do?pageNo={page+1}")
+                event_list = self.driver.find_elements(by=By.XPATH, value="//ul[@class='event_list']//li//a")
+                print(f"Find {len(event_list)} lists of event in page {page}")
+                
+                if event_list != None:   
+                    for event in event_list:
+                        airline = tag
+                        code = event.get_attribute('href')
+                        url = "https://flyairseoul.com/" + code
+                        header = event.find_element(by=By.CLASS_NAME, value="noti").get_attribute('innerText')
+                        content = event.find_element(by=By.CLASS_NAME, value="txt").get_attribute('innerText')
+                        date = event.find_element(by=By.CLASS_NAME, value="date").get_attribute('innerText')
+                        new_df = [(airline, url, date, header, content)]
+                        dfNew = pd.DataFrame(new_df, columns=['airline', 'url', 'date',  'header','content'])
+                        df = pd.concat([df,dfNew])
+                    print(df)
+                else:
+                    return
 
 
 #TEST
 # a= EventCrawler("https://www.jejuair.net/ko/event/event.do").getEventList()
 # a= EventCrawler("https://www.airbusan.com/content/common/flynjoy/flyNEvent/").getEventList()
 # a= EventCrawler("https://twayair.com/app/promotion/event/being?").getEventList()
+a= EventCrawler("https://flyairseoul.com/CW/ko/ingEvent.do").getEventList()
