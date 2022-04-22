0525ec809786cfffa57cf4869cd0f77854d8e774
Seunghyun-Kang <kiseyno@gmail.com>
2022-04-22 20:56:17 +0900

    Add Level2 module in main

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from datetime import date
 import pandas as pd
-from event_crawler import EventCrawler
+from event_crawler_level_1 import EventCrawlerLevel1
+from event_crawler_level_2 import EventCrawlerLevel2
 
 airTags = ['jinair', 'jejuair','airseoul','airbusan','tway']
-df = pd.DataFrame(columns=['airline', 'url', 'date',  'header','content'])  
+# airTags = ['jinair']
+df = pd.DataFrame(columns=['airline', 'url', 'date',  'header','content', 'level2', 'level3'])  
 
 for tag in airTags:
-    df = pd.concat([df,EventCrawler(tag).getEventList()])
-
+    df = pd.concat([df,EventCrawlerLevel1(tag).get_data_level_1()])
+    
 df = df.reset_index()
 df = df.drop('index', axis=1)
 print(df)
+
+for i in range(len(df.url)-1):
+    url = df.url.values[i]
+    airline = df.airline.values[i]
+    df['level2'].values[i] = EventCrawlerLevel2(url, airline).get_data_level_2()
+print(df)
+
+
 today = date.today().isoformat()
 file_name = f'AirLines_Event_{today}.xlsx'
 df.to_excel(file_name)
